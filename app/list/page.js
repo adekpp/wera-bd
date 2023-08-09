a825eedbb9db7944857b9360d542c53da1a71efa
Adrian <adekpp@gmail.com>
2023-08-09 18:21:08 +0200

    data fetching changes

--- app/list/page.js
+++ app/list/page.js
@@ -1,23 +1,19 @@
-import UsersList from "../components/UsersList";
 
 async function getData() {
-  const res = await fetch("https://wera-bd.vercel.app/guests", {
-    next: { revalidate: 0 },
-  });
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+  try {
+    const res = await prisma.name.findMany();
+    console.log(res);
+    return res;
+  } catch (e) {
+    console.log(e);
   }
-
-  return res.json();
 }
+import UsersList from "../components/UsersList";
+import prisma from "../../lib/prisma";
 
 export default async function List() {
-  const data = await getData();
 
+  const data = await getData()
   return (
     <div className="font-sans mt-6 text-gray-900 pb-40 w-full">
       <h1 className="text-xl mb-4">Lista gości: 🥳 🥳 🥳</h1>
